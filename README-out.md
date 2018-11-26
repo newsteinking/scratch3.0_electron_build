8310e53a54a9d5280fb23af834d5724618c83683
BuildTools <unconfigured@null.spigotmc.org>
2018-11-26 15:12:48 +0900

    dd

---
+++ README-out.md
@@ -0,0 +1,17 @@
+# Scratch3.0_build_out
+
+스크래치 3.0 electron build 입니다.
+
+win32,win64,Linux,Mac 버젼이 준비되어 있습니다.
+
+source base in https://github.com/LLK/scratch-gui
+
+
+# add exentension as same as microbit
+
+microbit extension 테스트를 위해 built in extension을 추가 했습니다.
+
+source base in https://github.com/newsteinking/SEANLABScratch3.0
+
+scratch-microbit-1.0.hex source는 입수하는 대로 올리겠습니다.
+
---
+++ deploy-sean-all.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# See https://medium.com/@nthgergo/publishing-gh-pages-with-travis-ci-53a8270e87db
+set -o errexit
+
+rm -rf out
+mkdir out
+
+# config
+git config --global user.email "newstein33@gmail.com"
+git config --global user.name "newsteinking"
+
+# build (CHANGE THIS)
+mkdir editor
+cd editor
+mkdir scratch-gui
+git clone https://github.com/newsteinking/SEANLABScratch3.0 scratch-gui-source
+cd scratch-gui-source
+npm install > /dev/null 2>&1
+npm run build > /dev/null 2>&1
+cp -r ./build/* ../
+cd ../
+rm -r -f "scratch-gui-source"
+
+cd ../
+echo "Win x64"
+./node_modules/.bin/electron-packager . "Scratch3.0" --asar --platform win32 --arch x64 --out out --icon seanlablogo.ico --prune false
+echo "Win x32"
+./node_modules/.bin/electron-packager . "Scratch3.0" --asar --platform win32 --arch ia32 --out out --icon seanlablogo.ico --prune false
+echo "Linux x64"
+./node_modules/.bin/electron-packager . "Scratch3.0" --asar --platform linux --arch x64 --out out --icon seanlablogo.png --prune false
+echo "Linux x32"
+./node_modules/.bin/electron-packager . "Scratch3.0" --asar --platform linux --arch ia32 --out out --icon seanlablogo.png --prune false
+echo "Linux ARM"
+./node_modules/.bin/electron-packager . "Scratch3.0" --asar --platform linux --arch armv7l --out out --icon seanlablogo.png --prune false
+echo "Mac x64"
+./node_modules/.bin/electron-packager . "Scratch3.0" --asar --platform darwin --arch x64 --out out --icon icon.icns --prune false
+
+# deploy
+cd out
+zip -r -q -9 Scratch3.0-Win32-x64.zip "Scratch3.0-win32-x64"
+rm -r "Scratch3.0-win32-x64"
+zip -r -q -9 Scratch3.0-Win32-ia32.zip "Scratch3.0-win32-ia32"
+rm -r "Scratch3.0-win32-ia32"
+tar -cvzf Scratch3.0-Linux-x64.tar.gz "Scratch3.0-linux-x64" > /dev/null 2>&1
+rm -r "Scratch3.0-linux-x64"
+tar -cvzf Scratch3.0-Linux-ia32.tar.gz "Scratch3.0-linux-ia32" > /dev/null 2>&1
+rm -r "Scratch3.0-linux-ia32"
+tar -cvzf Scratch3.0-Linux-armv7l.tar.gz "Scratch3.0-linux-armv7l" > /dev/null 2>&1
+rm -r "Scratch3.0-linux-armv7l"
+tar -cvzf Scratch3.0-Mac-x64.tar.gz "Scratch3.0-darwin-x64" > /dev/null 2>&1
+rm -r "Scratch3.0-darwin-x64"
+cp ../index.md ./
+cp ../_config.yml ./
+cp ../README.md ./
+
+git init
+git lfs install
+git lfs track *
+git add .
+git commit -m "Deploy to Github Pages"
+git push --force "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPO}.git" master:gh-pages
+exit 0
